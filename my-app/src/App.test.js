7d6893ad19750682479411e99e716bc2af5abba5
Yash Patel <yashpatel1815@hotmail.com>
2023-10-10 12:11:03 -0400

    Added tailwind and react-router-dom Made Navbar and temp home page

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import Navbar from "./components/Navbar";
+import { MemoryRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders navbar", () => {
+    render(
+        <MemoryRouter>
+            <Navbar />
+        </MemoryRouter>
+    );
+    // Check if company name and sign in button are there
+    const companyName = screen.getByText(/LT Real Estate/i);
+    expect(companyName).toBeInTheDocument();
+
+    const login = screen.getByText(/Login/i);
+    expect(login).toBeInTheDocument();
 });
