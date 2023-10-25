f15b9089e1e59feed627aa2ac86e18a80b37b104
Yash Patel <yashpatel1815@hotmail.com>
2023-10-25 19:39:10 -0400

    removed address in schema

--- my-app/backend/models/propertyModel.js
+++ my-app/backend/models/propertyModel.js
@@ -1,38 +1,41 @@
 import mongoose, { mongo } from "mongoose";
 
 // Making a schema for properties
-// Properties have a price, address and description
+// Properties have a price, address, description and image
 
 const propertySchema = mongoose.Schema({
     price: {
         type: Number,
         required: true,
     },
-    address: {
-        houseNumber: {
-            type: Number,
-            required: true,
-        },
-        street: {
-            type: String,
-            required: true,
-        },
-        city: {
-            type: String,
-            required: true,
-        },
-        province: {
-            type: String,
-            required: true,
-            maxLength: 2, // Only use abbreviations for provinces
-        },
-        postalCode: {
-            type: String,
-            required: true,
-            maxLength: 6,
-        },
+    houseNumber: {
+        type: Number,
+        required: true,
+    },
+    street: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    province: {
+        type: String,
+        required: true,
+        maxLength: 2, // Only use abbreviations for provinces
+    },
+    postalCode: {
+        type: String,
+        required: true,
+        maxLength: 6, // Canadian postal codes
     },
     desc: {
+        type: String,
+        required: true,
+    },
+    imageURL: {
+        // Link to image of property to be displayed on cards
         type: String,
         required: true,
     },
