4a556ce50c69e30620b2853af80fa12667107903
Siming Shen <simingshen97@gmail.com>
2023-10-26 08:26:33 -0400

    Login and close button working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import LoginPopup from "./components/LoginPopup";
+import { useState } from 'react';
 
 function App() {
+    const [buttonPopup, setButtonPopup] = useState(false);
     return (
         <div>
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
-            <LoginPopup trigger={true} />
-                
+
+            <button onClick={() => setButtonPopup(true)}>Login</button> 
+            
+            <LoginPopup trigger={buttonPopup} setTrigger = {setButtonPopup}/> 
+            
             <LoginPopup />
             
         </div>
--- my-app/src/components/LoginPopup.js
+++ my-app/src/components/LoginPopup.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './LoginPopup.css';
 
+
 function LoginPopup(props) {
+    
     return (props.trigger) ? (
         <div className='popup'>
+            
                 <div className='popup-inner'>
 
-                    <button className="close-btn">close</button>
+                    <button className="close-btn"onClick={() => props.setTrigger(false)}>close</button>
                     <p>Login</p>
                 </div> 
         </div>
