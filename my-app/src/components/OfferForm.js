35cd057ea000baa5c45b0343806e910f375c6faf
Siming Shen <simingshen97@gmail.com>
2023-11-04 01:48:53 -0400

    First completion of the offer form UI Need to add cookies to the form and connect to the backend

--- my-app/src/components/OfferForm.js
+++ my-app/src/components/OfferForm.js
@@ -1,60 +1,184 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+
+
 
 export const OfferForm = () => {
+    // storage for property and id
+    const [property, setProperty] = useState({});
+    const { id } = useParams();
+    // Method to get the property by id
+useEffect(() => {
+    axios
+        .get(`http://localhost:8080/api/properties/${id}`)
+        .then((res) => {
+            setProperty(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}, [id]);
+
+    
+                        
     return (
-        <div className="flex min-h-screen min-w-screen items-center justify-center">
-            <div className="flex-col max-w-md w-full">
-                <div>
-                    <h2 className="text-center text-3xl font-extrabold">
-                        IN THE WORKS. THIS IS JUST TO TEST THE ROUTE
-                    </h2>
-                </div>
-                <form className="flex-col mt-8">
+            <div className="flex min-h-fit min-w-screen items-center justify-center">
+                <div className="flex-col max-w-md w-full">
                     <div>
-                        <input
-                            id="username"
+                        <label className="block text-gray-700 text-3xl font-bold mb-2" htmlFor="date">
+                            Email Broker
+                        </label>
+                    </div>
+                    <form className="flex-col mt-8">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
+                            Your information
+                        </label>
+                        <div>
+                            <input
+                                id="first-name"
+                                type="text"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
+                                placeholder="First Name"
+                            />
+                        </div>
+                        <div>
+                            <input
+                                id="last-name"
+                                type="text"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
+                                placeholder="Last Name"
+                            />
+                        </div>
+                        <div>
+                            <input
+                                id="license-number"
+                                type="text"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
+                                placeholder="License Number"
+                            />
+                        </div>
+                        <div>
+                            <input
+                                id="Agency"
+                                type="text"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
+                                placeholder="Agency"
+                            />
+                        </div>
+                        {/* <div>
+                            <textarea
+                                id="message"
+                                type="text"
+                                rows={6}
+                                class="block p-2.5 w-full resize-none border border-gray-300 rounded-md placeholder-gray-500" 
+                                placeholder="Write custom Message"
+                            />
+                        </div>  */}
+                        <label 
+                            className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
+                            Buyer information
+                        </label>
+                        <div>
+                            <input
+                                id="first-name"
+                                type="text"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
+                                placeholder="First Name"
+                            />
+                        </div>
+                        <div>
+                            <input
+                                id="last-name"
+                                type="text"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
+                                placeholder="Last Name"
+                            />  
+                        </div>
+                        <div>
+                            <input
+                                id="email"
+                                type="email"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
+                                placeholder="you@example.com"
+                            />    
+                        </div>
+                        <div>
+                            <input
+                                id="address"
+                                type="tel"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
+                                placeholder="Address"
+                            />
+                        </div>       
+                        <div>
+                            <input
+                            id="address"
                             type="text"
-                            required
+                            value={property.houseNumber + ", " + property.street + "St, " + property.city + ", " + property.province + ", " + property.postalCode}
+                            disabled
                             className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
-                            placeholder="Username"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            id="email"
-                            type="email"
-                            required
-                            className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
-                            placeholder="Email"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            id="password"
-                            type="password"
-                            required
-                            className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div>
-                        <input
-                            id="confirm-password"
-                            type="password"
-                            required
-                            className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
-                            placeholder="Confirm Password"
-                        />
-                    </div>
-                    <div>
+                            />
+                            </div>  
+
+                        <div>
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
+                                Offered price
+                            </label>
+                            <input
+                                id="price"
+                                type="number"
+                                min="1"
+                                step="any"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500">
+                                </input>
+
+                        </div>
+                        <div>
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
+                                Deed of sale date
+                            </label>
+                            <input
+                                id="date"
+                                type="date"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
+                                
+                            />
+                        </div>
+                        <div>
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
+                                Occupancy of premises date
+                            </label>
+                            <input
+                                id="date"
+                                type="date"
+                                required
+                                className="w-full px-3 py-2 my-2 border border-gray-300 placeholder-gray-500"
+                                
+                            />
+                        </div>
+                        
+
+                    </form>
+                    
                         <button className="flex w-full mt-6 justify-center py-3 px-4 text-sm font-medium rounded-md text-white bg-blue-600">
-                            Submit 
+                            Submit
                         </button>
-                    </div>
-                </form>
+                    
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    };
 
 export default OfferForm;
